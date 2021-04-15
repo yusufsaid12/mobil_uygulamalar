5f3b0ba0f29a41fb10796827bcf025551baf3069
Merve <mervetemizer.meb@yahoo.com>
2021-04-16 02:33:35 +0300

    Add sinav_hazirlik

---
+++ sinav_hazirlik/sand_clock_pattern/main.dart
@@ -0,0 +1,202 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BenimUyg());
+}
+
+class BenimUyg extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey[400],
+        
+        appBar: AppBar(
+          backgroundColor: Colors.grey[100],
+          title: Text('Karşılama', style: TextStyle(fontWeight: FontWeight.bold, color:Colors.grey[500])),
+        ),
+        
+        body: SafeArea(
+            child:Column(
+              children:<Widget>[
+              Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children:<Widget>[
+              Container(
+                    padding: EdgeInsets.all(20.0),
+                    color: Colors.grey[100],
+                    width: 40,
+                    height: 80,
+                  ),
+              Container(
+                    padding: EdgeInsets.all(20.0),
+                    color: Colors.grey[100],
+                    width: 40,
+                    height: 80,
+                  ),
+            
+            Container(
+                    padding: EdgeInsets.all(20.0),
+                    color: Colors.grey[100],
+                    width: 40,
+                    height: 80,
+                  ),
+            
+            Container(
+                    padding: EdgeInsets.all(20.0),
+                    color: Colors.grey[100],
+                    width: 40,
+                    height: 80,
+                  ),
+            
+            Container(
+                    padding: EdgeInsets.all(20.0),
+                    color: Colors.grey[100],
+                    width: 40,
+                    height: 80,
+                  ),
+            
+              
+              ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children:<Widget>[
+              Container(
+                
+                    padding: EdgeInsets.all(20.0),
+                    color: Colors.grey[100],
+                    width: 40,
+                    height: 80,
+                  ),
+              Container(
+                    padding: EdgeInsets.all(20.0),
+                    color: Colors.grey[100],
+                    width: 40,
+                    height: 80,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(20.0),
+                    color: Colors.grey[100],
+                    width: 40,
+                    height: 80,
+                  ),
+              Container(
+                    padding: EdgeInsets.all(20.0),
+                    color: Colors.grey[100],
+                    width: 40,
+                    height: 80,
+                  ),
+            
+            
+              
+              ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children:<Widget>[
+              Container(
+                
+                    padding: EdgeInsets.all(20.0),
+                    color: Colors.grey[100],
+                    width: 40,
+                    height: 80,
+                  ),
+              Container(
+                    padding: EdgeInsets.all(20.0),
+                    color: Colors.grey[100],
+                    width: 40,
+                    height: 80,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(20.0),
+                    color: Colors.grey[100],
+                    width: 40,
+                    height: 80,
+                  ),
+                  ],
+                  ),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children:<Widget>[
+              Container(
+                
+                    padding: EdgeInsets.all(20.0),
+                    color: Colors.grey[100],
+                    width: 40,
+                    height: 80,
+                  ),
+              Container(
+                    padding: EdgeInsets.all(20.0),
+                    color: Colors.grey[100],
+                    width: 40,
+                    height: 80,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(20.0),
+                    color: Colors.grey[100],
+                    width: 40,
+                    height: 80,
+                  ),
+              Container(
+                    padding: EdgeInsets.all(20.0),
+                    color: Colors.grey[100],
+                    width: 40,
+                    height: 80,
+                  ),
+                
+            
+              
+              ],
+              ),
+              Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children:<Widget>[
+              Container(
+                    padding: EdgeInsets.all(20.0),
+                    color: Colors.grey[100],
+                    width: 40,
+                    height: 80,
+                  ),
+              Container(
+                    padding: EdgeInsets.all(20.0),
+                    color: Colors.grey[100],
+                    width: 40,
+                    height: 80,
+                  ),
+            
+            Container(
+                    padding: EdgeInsets.all(20.0),
+                    color: Colors.grey[100],
+                    width: 40,
+                    height: 80,
+                  ),
+            
+            Container(
+                    padding: EdgeInsets.all(20.0),
+                    color: Colors.grey[100],
+                    width: 40,
+                    height: 80,
+                  ),
+            
+            Container(
+                    padding: EdgeInsets.all(20.0),
+                    color: Colors.grey[100],
+                    width: 40,
+                    height: 80,
+                  ),
+            
+              
+              ],
+              ),
+              ],
+            ),
+          ),
+        ),
+      
+    );
+  }
+}
+
+
---
+++ sinav_hazirlik/with_padding/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BenimUyg());
+}
+
+class BenimUyg extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey[600],
+        
+        appBar: AppBar(
+          backgroundColor: Colors.grey[100],
+          title: Text('Karşılama', style: TextStyle(fontWeight: FontWeight.bold, color:Colors.grey[500])),
+        ),
+        
+        body: SafeArea(
+            child:Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children:<Widget>[
+				Container(
+				color: Colors.grey[200],
+					child: Container(
+            decoration: BoxDecoration(
+              color: Colors.grey[400] ,
+            border: Border.all(color: Colors.black, width: 10,)
+            ),
+						padding: EdgeInsets.all(50),
+						child: Container(
+							alignment: Alignment.center,
+							color: Colors.white,
+							width: 240,
+							height: 80,
+							child:Text('Merhaba Flutter!', style: TextStyle(fontSize:24, color: Colors.grey[800])),
+                      ),
+                ),
+				),
+                      
+                ],
+              ),
+            ),
+          ),
+        ),
+      
+    );
+  }
+}
+
+
